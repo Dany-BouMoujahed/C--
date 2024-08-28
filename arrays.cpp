3faa00f7f0e4ebac0e10d79e083504ed0772b46b
Dany Bou Moujahed <Danyboumoujahed@gmail.com>
2024-08-28 21:51:28 +0300

    added loops to iterate over array

--- arrays.cpp
+++ arrays.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 int main() {
     int a[4] = {1,2,3,4};
-    cout << a[1] << endl;
+    for (int i=0;i < (sizeof(a)/sizeof(a[0])); i++){
+    cout << a[i] << " ";
+    }
     return 0;
 }
