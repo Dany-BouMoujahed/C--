f6d01756d3b897d65ded8eafa18e96db5f879f5c
Dany Bou Moujahed <Danyboumoujahed@gmail.com>
2024-09-14 22:47:58 +0300

    finished p2

--- ps1/p2.cpp
+++ ps1/p2.cpp
@@ -10,6 +10,10 @@ int main(){
     double hm;
     double fam;
     double a1;
+    double a2;
+    double a3;
+    double a4;
+    double a5;
     double b;
     double bodyfat;
     double bodyfatpercent;
@@ -20,6 +24,33 @@ int main(){
         cin >> bw;
         cout << "wrm= ";
         cin >> wrm;
+        a1 = (bw*1.082)*+94.42;
+        a2 = wrm*4.15;
+        b = a1-a2;
+        bodyfat = bw-b;
+        bodyfatpercent = (bodyfat/bw)*100;
     }
+    else {
+        cout << "bw= ";
+        cin >> bw;        
+        cout << "wrm= ";
+        cin >> wrm;        
+        cout << "wam= ";
+        cin >> wam;        
+        cout << "hm= ";
+        cin >> hm;        
+        cout << "fam= ";
+        cin >> fam;
+        a1 = (bw*0.732)+8.987;
+        a2 = wrm/3.140;
+        a3 = wam*0.157;
+        a4 = hm*0.249;
+        a5 = fam*0.434;
+        b = a1+a2-a3-a4+a5;
+        bodyfat=bw-b;
+        bodyfatpercent=(bodyfat/bw)*100;
+    }
+    cout << "body fat = " << bodyfat << endl;
+    cout << "body fat % = " << bodyfatpercent << endl; 
     return 0;
 }
